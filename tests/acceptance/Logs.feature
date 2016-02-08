04f217a9a15ad51dbc27041ef5da85403daf8597
yuriikrevnyi <yuriikrevnyi@gmail.com>
2016-02-08 15:57:39 +0200

    Add tests for reports

--- tests/acceptance/Logs.feature
+++ tests/acceptance/Logs.feature
@@ -7,3 +7,8 @@ Feature: Logs
     Given I do not have any logs
     When I log some 'message'
     Then I should see that 'message' in the log file
+
+  Scenario: Can write reports
+    Given I do not have 'testing' report
+    When I report 'test' message
+    Then I should see that 'test' message in the report file
--- tests/acceptance/step_definitions/Logs/index.js
+++ tests/acceptance/step_definitions/Logs/index.js
@@ -6,7 +6,7 @@ var request = require('request');
 
 var Logs = function Logs() {
 
-  this.Before(function() {
+  this.Before(function () {
     this.randomNumber = Math.random();
   });
 
@@ -22,6 +22,33 @@ var Logs = function Logs() {
 
   this.Then(/^I should see that 'message' in the log file$/, function () {
     infoLogHas(`Testing message ${this.randomNumber}`).should.be.ok();
+  });
+
+  // Reports
+  this.Given(/^I do not have 'testing' report$/, function (callback) {
+    var files = fs.readdirSync(basePath('storage/reports'));
+
+    if (~files.indexOf('testing.log')) {
+      fs.unlinkSync(basePath('storage/reports/testing.log'));
+    }
+
+    setTimeout(() => callback(), 1000);
+  });
+
+  this.When(/^I report 'test' message$/, function (callback) {
+    report('testing', 'foo', this.randomNumber);
+
+    setTimeout(() => callback(), 1000);
+  });
+
+  this.Then(/^I should see that 'test' message in the report file$/, function () {
+    var report = fs.readFileSync(basePath(`storage/reports/testing.log`), 'utf8');
+
+    var regexp1 = new RegExp('foo');
+    var regexp2 = new RegExp(this.randomNumber);
+
+    regexp1.test(report).should.be.ok();
+    regexp2.test(report).should.be.ok();
   });
 
   function infoLogHas(str) {
